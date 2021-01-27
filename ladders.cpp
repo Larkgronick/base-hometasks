9861fbe6ed11a2bcd57581db65a78cbe2f591bef
Nikita Zhavoronkov <larkgronick@gmail.com>
2021-01-27 14:31:49 +0300

    feat: solve remove repeated words / third ladder

--- ladders.cpp
+++ ladders.cpp
@@ -3,15 +3,42 @@ using namespace std;
 #define N 128
 
 bool isSeparator(char c);
+void showWordAmount(char text[]);
+void showMostRepeatedChar(char text[]);
+
 
 int main()
 {
-   char text[N], word[N], maxWord[N]; 
-   int i, iw = 0, wordAmount = 0, maxLength = 0, wordLength = 0, startIndex = 0;
+   char text[N];
 
    cout << "Input text: ";
    cin.getline(text, N);
    
+   showWordAmount(text);
+   showMostRepeatedChar(text);
+   
+   return 0;
+}
+
+bool isSeparator(char c)
+{
+   char s[] = ",.! ?";
+   for (int i = 0; s[i] != '\0'; i++)
+   {
+      if(c == s[i])
+      {
+         return true;
+      }
+   }
+   return false;
+   
+}
+
+void showWordAmount(char text[])
+{
+   char word[N];
+   int i, iw, wordLength,  maxLength, startIndex, wordAmount  = 0;
+
    for (i = 0; text[i] != '\0'; i++)
    {
       if (!isSeparator(text[i]))
@@ -28,33 +55,41 @@ int main()
               startIndex = i+1 - maxLength; 
            }
            word[iw] = '\0';
-           cout << word << endl;
            iw = 0;
            wordAmount++;
-           wordLength = 0;
         }
       }    
    }
-   cout << wordAmount << endl;
-   cout << maxLength << endl;
-   cout << startIndex << endl;
-   for (i = startIndex; i < maxLength + startIndex; i++)
+
+   cout << "Word amount: " << wordAmount << endl;
+   cout << "Largest word: ";
+
+   for (i = startIndex; i < startIndex + maxLength; i++) 
    {
-     cout << text[i];
-   }
-    return 0;
+			cout << text[i];
+	}
+
+   cout << endl;
+   
 }
 
-bool isSeparator(char c)
+void showMostRepeatedChar(char text[])
 {
-   char s[] = ",.! ?";
-   for (int i = 0; s[i] != '\0'; i++)
-   {
-      if(c == s[i])
-      {
-         return true;
-      }
-   }
-   return false;
-   
+   char repeatedChar;
+   int i, repeat, max = 0;
+
+   	for (i = 0; text[i] != '\0'; i++) {
+		   for (int j = 0; text[j] != '\0'; j++) {
+		   	if (text[i] == text[j]) {
+		   		repeat++;
+		   		if (repeat > max) {
+		   			max = repeat;
+		   			repeatedChar = text[i];
+		   		}
+		   	}
+		   }
+		   repeat = 0;
+	   }
+
+   cout << "Most repeated character is: " << repeatedChar << endl;
 }
